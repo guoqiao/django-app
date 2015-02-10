4ed7972d8e43c6e6c106222ad46cb11db1c23ee0
guoqiao <guoqiao@gmail.com>
2015-02-11 00:26:51 +1300

    rm proj name in uwsgi.py

--- example/wsgi.py
+++ example/wsgi.py
@@ -1,14 +1,5 @@
-"""
-WSGI config for example project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
