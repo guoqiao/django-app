ac4af6f852133cccf3550acf337c916dd0de4346
guoqiao <guoqiao@gmail.com>
2015-02-11 16:49:13 +1300

    fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-nzpower',
-    version='0.1',
+    version='0.2',
     packages=['nzpower'],
+    include_package_data=True,
     license='MIT',
     description='A simple Django app.',
     long_description=README,
